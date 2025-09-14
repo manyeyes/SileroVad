53e31aee57db20586dfbb245dc94fa0a3e5790f6
manyeyes <zx.520.521@126.com>
2025-09-14 23:42:48 +0800

    code formating

--- SileroVad/Model/ModelCustomMetadata.cs
+++ SileroVad/Model/ModelCustomMetadata.cs
@@ -1,10 +1,6 @@
 ﻿// See https://github.com/manyeyes for more information
 // Copyright (c)  2023 by manyeyes
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using YamlDotNet.Serialization;
 
 namespace SileroVad.Model
 {
